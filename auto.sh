99535dfb1f7412c18cd30605e075c69391370272
River-Shi <nachuan.shi.quant@gmail.com>
2025-07-20 03:49:30 +0000

    Add environment configuration for Redis and Grafana; streamline auto.sh and install_trzsz.sh

--- auto.sh
+++ auto.sh
@@ -4,7 +4,7 @@
 sudo apt-get update
 
 # 安装必要的系统依赖
-sudo apt-get install -y build-essential npm autoconf automake libtool python3-dev
+sudo apt-get install -y build-essential npm
 
 # 安装 npm 和 pm2
 sudo npm install -g pm2
@@ -15,12 +15,3 @@ source $HOME/.local/bin/env
 
 curl -fsSL https://test.docker.com -o test-docker.sh
 sudo sh test-docker.sh
-
-sudo sh install_talib.sh
-
-docker compose up -d grafana
-
-sudo apt update && sudo apt install software-properties-common
-sudo add-apt-repository ppa:trzsz/ppa && sudo apt update
-
-sudo apt install trzsz
---
+++ install_trzsz.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt update && sudo apt install software-properties-common
+sudo add-apt-repository ppa:trzsz/ppa && sudo apt update
+
+sudo apt install trzsz
